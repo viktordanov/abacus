606fac100e596335c6e0040fa8418b5ac7aecd48
Viktor Danov <orangeleaf12@gmail.com>
2021-01-19 19:33:31 +0200

    implement new cli with history and support for suggestions

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/viktordanov/abacus
 
 go 1.15
 
-require github.com/antlr/antlr4 v0.0.0-20210114010855-d34d2e1c271a
+require (
+	github.com/antlr/antlr4 v0.0.0-20210114010855-d34d2e1c271a
+	github.com/fatih/color v1.10.0
+	github.com/peterh/liner v1.2.1
+	github.com/pkg/term v1.1.0
+)
--- main.go
+++ main.go
@@ -1,31 +1,69 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"github.com/fatih/color"
+	"github.com/peterh/liner"
 	"github.com/viktordanov/abacus/parser"
-	"math/big"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"math/big"
 )
 
+var historyFile = filepath.Join(os.TempDir(), ".abacus_history")
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 	visitor := NewAbacusVisitor()
-	for {
-		fmt.Print("> ")
-		input, _ := reader.ReadString('\n')
+	line := liner.NewLiner()
+	answerPrinter := color.New(color.FgGreen)
+	defer line.Close()
 
-		is := antlr.NewInputStream(input)
-		lexer := parser.NewAbacusLexer(is)
-		stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
-
-		p := parser.NewAbacusParser(stream)
-		tree := p.Start()
-		ans := visitor.Visit(tree)
-		switch val := ans.(type) {
-		case *big.Float:
-			fmt.Println(val.Text('g', 256))
-		}
+	line.SetCtrlCAborts(true)
+	if f, err := os.Open(historyFile); err == nil {
+		line.ReadHistory(f)
+		f.Close()
 	}
+
+	if f, err := os.Create(historyFile); err != nil {
+		log.Print("Error writing history file: ", err)
+	} else {
+		line.WriteHistory(f)
+		f.Close()
+	}
+
+	for {
+		if name, err := line.Prompt("> "); err == nil {
+			line.AppendHistory(name)
+			is := antlr.NewInputStream(name)
+			lexer := parser.NewAbacusLexer(is)
+			stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+			p := parser.NewAbacusParser(stream)
+			tree := p.Start()
+			ans := visitor.Visit(tree)
+			switch val := ans.(type) {
+			case *big.Float:
+				answerPrinter.Println(val.Text('g', 256))
+			}
+		} else if err == liner.ErrPromptAborted {
+			os.Exit(0)
+		} else {
+			log.Print("Error reading line: ", err)
+		}
+
+	}
+}
+
+func updateCompletions(line *liner.State, completions []string) {
+	line.SetCompleter(func(line string) (c []string) {
+		for _, n := range completions {
+			if strings.HasPrefix(n, strings.ToLower(line)) {
+				c = append(c, n)
+			}
+		}
+		return
+	})
 }
