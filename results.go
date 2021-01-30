c17c96ca4ddffa4bf4fb7c325f095f5ec2acfb2f
Viktor Danov <orangeleaf12@gmail.com>
2021-01-30 20:42:29 +0200

    reimplement data flow through the parse tree

--- results.go
+++ results.go
@@ -1,38 +1,157 @@
 package main
 
 import (
+	"errors"
 	"github.com/cockroachdb/apd"
+	"strconv"
+	"strings"
 )
 
-type ResultAssignment struct {
-	Values []*apd.Decimal
+// Result is struct returned after evaluating each leaf in the parser tree. 
+// Errors are propagated up 
+type Result struct {
+	Value  Formatter
+	Errors []error
+}
+
+func NewResult(value Formatter) *Result {
+	return &Result{Value: value, Errors: make([]error, 0)}
+}
+func (r *Result) WithErrors(previous *Result, ee ...string) *Result {
+	if previous != nil {
+		for _, e := range previous.Errors {
+			r.Errors = append(r.Errors, e)
+		}
+	}
+	for _, e := range ee {
+		r.Errors = append(r.Errors, errors.New(e))
+	}
+	return r
+}
+
+func decimalsToStringList(dd []ResultNumber) string {
+	var b strings.Builder
+	count := len(dd)
+	if count > 1 {
+		b.WriteRune('(')
+	}
+	for i, d := range dd {
+		b.WriteString(d.Text('g'))
+		if i+1 != count {
+			b.WriteString(", ")
+		}
+	}
+
+	if count > 1 {
+		b.WriteRune(')')
+	}
+	return b.String()
+}
+
+func stringsToStringList(ss []ResultString) string {
+	var b strings.Builder
+	count := len(ss)
+	if count > 1 {
+		b.WriteRune('(')
+	}
+	for i, s := range ss {
+		b.WriteString(string(s))
+		if i+1 != count {
+			b.WriteString(", ")
+		}
+	}
+
+	if count > 1 {
+		b.WriteRune(')')
+	}
+	return b.String()
+}
+
+func addColor(str string, color Color) string {
+	var b strings.Builder
+	b.WriteString(string(color))
+	b.WriteString(str)
+	b.WriteString(string(Reset))
+	return b.String()
+}
+
+type ResultAssignment []ResultNumber
+
+func (r ResultAssignment) String() string {
+	return decimalsToStringList(r)
+}
+
+func (r ResultAssignment) Color() string {
+	return addColor(r.String(), Magenta)
 }
 
 type ResultLambdaAssignment string
 
-type ResultTuple struct {
-	Values []*apd.Decimal
+func (r ResultLambdaAssignment) String() string {
+	return string(r)
+}
+func (r ResultLambdaAssignment) Color() string {
+	return addColor(r.String(), Magenta)
 }
 
-type ResultVariablesTuple struct {
-	Variables []string
-}
-type ResultError string
+type ResultTuple []ResultNumber
 
-func NewResultAssignment() ResultAssignment {
-	return ResultAssignment{
-		Values: make([]*apd.Decimal, 0),
+func (r ResultTuple) String() string {
+	return decimalsToStringList(r)
+}
+
+func (r ResultTuple) Color() string {
+	return addColor(r.String(), Green)
+}
+
+type ResultVariablesTuple []ResultString
+
+func (r ResultVariablesTuple) String() string {
+	return stringsToStringList(r)
+}
+
+func (r ResultVariablesTuple) Color() string {
+	return addColor(r.String(), Magenta)
+}
+
+type ResultNumber struct {
+	*apd.Decimal
+}
+
+func (r ResultNumber) String() string {
+	return r.Text('g')
+}
+
+func (r ResultNumber) Color() string {
+	return addColor(r.String(), Green)
+}
+
+type ResultBool bool
+
+func (r ResultBool) String() string {
+	return strconv.FormatBool(bool(r))
+}
+func (r ResultBool) Color() string {
+	return addColor(r.String(), Green)
+}
+
+type ResultString string
+
+func (r ResultString) String() string {
+	return string(r)
+}
+func (r ResultString) Color() string {
+	return addColor(r.String(), Green)
+}
+
+// Length returns the length of the tuple value of result.
+// If Value is not a tuple type 1 is returned
+func (r *Result) Length() int {
+	switch val := r.Value.(type) {
+	case ResultVariablesTuple:
+		return len(val)
+	case ResultTuple:
+		return len(val)
 	}
-}
-
-func NewResultTuple() ResultTuple {
-	return ResultTuple{
-		Values: make([]*apd.Decimal, 0),
-	}
-}
-
-func NewResultVariablesTuple() ResultVariablesTuple {
-	return ResultVariablesTuple{
-		Variables: make([]string, 0),
-	}
+	return 1
 }
